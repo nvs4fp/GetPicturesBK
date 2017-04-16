5a7296df0b6d546c14ab7fc0a2ecd68df94e1616
LiYan <robotdev@126.com>
2017-04-16 19:00:01 +0800

    Add history.

---
+++ FileTest.py
@@ -0,0 +1,20 @@
+#Initialized by Levi Li
+#Date: 19-Apr-2017
+#Discription: Just for the study
+#History
+#--------------------------------------------------------
+#Date               Useer                   Discription
+#19-Apr-
+#19-Apr-2017        Levi Li                 Create
+#--------------------------------------------------------
+'''
+<img class="main_img img-hover" data-imgurl="https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=4225527214,2698476519&amp;fm=23&amp;gp=0.jpg" src="https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=4225527214,2698476519&amp;fm=23&amp;gp=0.jpg" style="background-color: rgb(182, 173, 173); width: 295px; height: 197px;">
+
+'''
+
+import requests
+if __name__ == "__main__":
+    print('Hello python!')
+    res=requests.get(r"https://image.baidu.com/search/index?tn=baiduimage&ipn=r&ct=201326592&cl=2&lm=-1&st=-1&fm=result&fr=&sf=1&fmq=1492305816848_R&pv=&ic=0&nc=1&z=&se=1&showtab=0&fb=0&width=&height=&face=0&istype=2&ie=utf-8&word=%E6%A1%83%E8%8A%B1")
+    print(res.content)
+
---
+++ theard.py
@@ -0,0 +1,32 @@
+#coding=utf-8
+from time import ctime , sleep
+import threading
+
+def Process1():
+    for i in range(100):
+        print("c1 %s"%(ctime()))
+        print(threading.current_thread())
+        sleep(0.3)
+
+def Process2():
+    for i in range(100):
+        print("c2 %s"%(ctime()))
+        sleep(0.5)
+
+def OneThreading():
+    Process1()
+    Process2()
+
+def MultiThreading():
+    threads=[]
+    t1=threading.Thread(target=Process1())
+    #threads.append(t1)
+    t2=threading.Thread(target=Process2())
+    #threads.append(t2)
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
+if __name__=='__main__':
+    #OneThreading()
+    MultiThreading()
