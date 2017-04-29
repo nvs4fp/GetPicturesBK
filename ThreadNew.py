b6f723917ee940a60697a58e11a2b0b204b67af4
LiYan <robotdev@126.com>
2017-04-29 16:44:10 +0800

    Add self created class for multiple threads study. This is a good start, i think multiple threads will very usefully.

--- ThreadNew.py
+++ ThreadNew.py
@@ -4,6 +4,7 @@
 #History
 #Date           User                History
 #16-Apr-2017    Levi Li             Initial
+#29-Apr-2017    Levi Li             Add class myThreadControl
 #-----------------------------------------------------------------------------------------------------
 import threading
 import time
@@ -29,12 +30,45 @@ def print_time(threadName, delay, counter):
         print( "%s: %s" % (threadName, time.ctime(time.time())))
         counter -= 1
 
-# 创建新线程
-thread1 = myThread(1,"Thread-1", 1)
-thread2 = myThread(2, "Thread-2", 2)
+def thread_control_sample():
+    '''
+    This sample is copy from web.
+    :return:
+    '''
+    # 创建新线程
+    thread1 = myThread(1,"Thread-1", 1)
+    thread2 = myThread(2, "Thread-2", 2)
 
-# 开启线程
-thread1.start()
-thread2.start()
+    # 开启线程
+    thread1.start()
+    thread2.start()
+    print("Exiting Main Thread")
 
-print("Exiting Main Thread")
+
+'''
+The following classes and method are creaded by myself
+'''
+class myThreadControl(threading.Thread):
+    def __init__(self,threadName, printCounts, sleepTime):
+        threading.Thread.__init__(self)
+        self.threadName=threadName
+        self.sleepTime=sleepTime
+        self.printCount=printCounts
+    def run(self):
+        printNameByThread(self.threadName,self.printCount,self.sleepTime)
+
+def printNameByThread(threadName,printCount,sleepTime):
+    idx=1
+    while(idx<=printCount):
+        print("This is %d times printing by thread %s"%(idx,threadName))
+        idx+=1
+        time.sleep(sleepTime)
+
+def thread_control_self_created():
+    slowThread = myThreadControl("slow", 5, 1.5)
+    fastThread = myThreadControl("fast", 5, 1)
+    slowThread.start()
+    fastThread.start()
+if __name__=="__main__":
+    #thread_control_sample()
+    thread_control_self_created()
