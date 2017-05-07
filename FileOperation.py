e20292072d656fee94c2632ee9f19d16b2fbf023
LiYan <robotdev@126.com>
2017-05-07 10:56:38 +0800

    Add File Operation class
    Implement line number adding function

--- FileOperation.py
+++ FileOperation.py
@@ -1,9 +1,44 @@
 # To study how to operate file
+import re
+
+class fileOperation(object):
+
+    def __init__(self, fileName):
+        self.fileName=fileName
+
+    def add_line_index(self):
+        f = open(self.fileName, 'r')
+        if self.__check_index_existing(f.readline()):
+            f.close()
+            return
+        else:
+            f.close()
+
+        f = open(self.fileName, 'r')
+        m_new_lines = []
+        m_line_num = 1
+        for line in f.readlines():
+            if line:
+                m_new_lines.append("%d." % m_line_num + line)
+                m_line_num += 1
+        f.close()
+
+        f = open(self.fileName, 'w')
+        for line in m_new_lines:
+            f.write(line)
+        f.close()
+
+    def __check_index_existing(self, lines):
+        if len(lines)<1:
+            return False
+        if re.findall('\d+\.', lines):
+            return True
+
+
+def file_read_write():
+    file_op=fileOperation("finput.txt")
+    file_op.add_line_index()
+
 if __name__=="__main__":
-    fp = "finput.txt"
-    f = open(fp,'w')
-    f.write("This is a test file!")
-    f.close()
-    f = open(fp, 'r')
-    print(f.read())
-    f.close()
+    file_read_write()
+
